a1802912c84b174d8cbe592b4869fc3b5a46f465
Irsyad Fikriansyah <irsyadfikriansyah@gmail.com>
2023-09-04 10:47:49 +0700

    first commit

---
+++ SimpleCalculator/ViewController.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Globalization;
+
+using AppKit;
+using Foundation;
+
+namespace SimpleCalculator
+{
+	public partial class ViewController : NSViewController
+	{
+		public ViewController (IntPtr handle) : base (handle)
+		{
+		}
+
+		public override void ViewDidLoad ()
+		{
+			base.ViewDidLoad ();
+
+			// Do any additional setup after loading the view.
+		}
+
+		public override NSObject RepresentedObject {
+			get {
+				return base.RepresentedObject;
+			}
+			set {
+				base.RepresentedObject = value;
+				// Update the view, if already loaded.
+			}
+		}
+
+		private float getNumber(NSTextField x)
+		{
+			return float.Parse(x.StringValue, CultureInfo.InvariantCulture.NumberFormat);
+        }
+
+        partial void clearButton(NSObject sender)
+		{
+			num1.StringValue = @"";
+			num2.StringValue = @"";
+			result.StringValue = @"";
+        }
+
+        partial void divButton(NSObject sender)
+		{
+			float ans = getNumber(num1) / getNumber(num2);
+			result.StringValue = ans.ToString();
+        }
+
+        partial void minButton(NSObject sender)
+		{
+            float ans = getNumber(num1) - getNumber(num2);
+            result.StringValue = ans.ToString();
+        }
+
+        partial void plusButton(NSObject sender)
+		{
+            float ans = getNumber(num1) + getNumber(num2);
+            result.StringValue = ans.ToString();
+        }
+
+        partial void timesButton(NSObject sender)
+		{
+            float ans = getNumber(num1) * getNumber(num2);
+            result.StringValue = ans.ToString();
+        }
+    }
+}
---
+++ SimpleCalculator/ViewController.designer.cs
@@ -0,0 +1,57 @@
+﻿// WARNING
+//
+// This file has been generated automatically by Visual Studio to store outlets and
+// actions made in the UI designer. If it is removed, they will be lost.
+// Manual changes to this file may not be handled correctly.
+//
+using Foundation;
+using System.CodeDom.Compiler;
+
+namespace SimpleCalculator
+{
+	[Register ("ViewController")]
+	partial class ViewController
+	{
+		[Outlet]
+		AppKit.NSTextField num1 { get; set; }
+
+		[Outlet]
+		AppKit.NSTextField num2 { get; set; }
+
+		[Outlet]
+		AppKit.NSTextField result { get; set; }
+
+		[Action ("clearButton:")]
+		partial void clearButton (Foundation.NSObject sender);
+
+		[Action ("divButton:")]
+		partial void divButton (Foundation.NSObject sender);
+
+		[Action ("minButton:")]
+		partial void minButton (Foundation.NSObject sender);
+
+		[Action ("plusButton:")]
+		partial void plusButton (Foundation.NSObject sender);
+
+		[Action ("timesButton:")]
+		partial void timesButton (Foundation.NSObject sender);
+		
+		void ReleaseDesignerOutlets ()
+		{
+			if (num1 != null) {
+				num1.Dispose ();
+				num1 = null;
+			}
+
+			if (num2 != null) {
+				num2.Dispose ();
+				num2 = null;
+			}
+
+			if (result != null) {
+				result.Dispose ();
+				result = null;
+			}
+		}
+	}
+}
